78126feb0021edc90cf5826aed885efb30a2de6c
pedrofreitaas <freitaas.pedro@gmail.com>
2023-02-03 17:58:54 -0300

    added movement for the enemy

--- game_name/entities/enemies/enemy.py
+++ game_name/entities/enemies/enemy.py
@@ -9,15 +9,65 @@ class Enemy(ent.Entity):
         
         ent.Entity.enemies.append(self)
 
+        self.movement_behavior: float = 0
+        self.upd_mov_behavior_coeficient: float = 1
+
+        self.random_move_interval = [0,30]
+        self.seek_player_interval = [30,70]
+        self.distance_player_interval = [70,90]
+        self.idle_interval = [90,100]
+
+    def updateMoveBehavior(self, coeficient: float) -> None:
+        '''Updates the variable that controls the movement behavior.\n'''
+        self.movement_behavior += coeficient * ent.Entity.dt
+
+        if self.movement_behavior >= 100: self.movement_behavior = 0
+        if self.movement_behavior < 0: self.movement_behavior = 0
+
+    def controlMovement(self) -> None:
+        '''Sets enemy movement, with a little bit of randness.\n'''
+        self.updateMoveBehavior(self.upd_mov_behavior_coeficient)
+
+        if self.randomizer.randint(1,50000) <= 5:
+            self.movement_behavior = self.randomizer.random() * 100
+
+        if ent.inInterval(self.random_move_interval, self.movement_behavior):
+            self.setRandomSpeed()
+            return
+
+        if ent.inInterval(self.seek_player_interval, self.movement_behavior):
+            self.setSeekPlayerSpeed()
+            return
+
+        if ent.inInterval(self.distance_player_interval, self.movement_behavior):
+            self.setDistancePlayerSpeed()
+            return
+
+        if ent.inInterval(self.idle_interval, self.movement_behavior):
+            self.speed_dir = ent.pg.math.Vector2()
+            return
+
     def setRandomSpeed(self) -> None:
         '''Sets the enemy speed randomly.\n'''
         if self.randomizer.randint(1,10000) > 20: return
 
-        self.speed: ent.pg.math.Vector2 = ent.pg.math.Vector2(self.randomizer.random(), self.randomizer.random())
-        if self.speed != ent.pg.math.Vector2(): self.speed = self.speed.normalize()
+        self.speed_dir: ent.pg.math.Vector2 = ent.pg.math.Vector2(self.randomizer.random()*self.randomizer.randint(-1,1), self.randomizer.random()*self.randomizer.randint(-1,1))
+        if self.speed_dir != ent.pg.math.Vector2(): self.speed_dir = self.speed_dir.normalize()
+    
+    def setSeekPlayerSpeed(self) -> None:
+        '''Sets the direction of movement to be towards the player.\n'''
+        distance = self.pos - ent.Entity.player.pos
+        if distance != ent.pg.math.Vector2(): self.speed_dir = distance.normalize()
+        else: self.speed_dir = ent.pg.math.Vector2()
+
+    def setDistancePlayerSpeed(self) -> None:
+        '''Sets the direction of movement to run to get far away from the player.\n'''
+        distance = ent.Entity.player.pos - self.pos
+        if distance != ent.pg.math.Vector2(): self.speed_dir = distance.normalize()
+        else: self.speed_dir = ent.pg.math.Vector2()
 
     def update(self) -> None:
-        self.setRandomSpeed()
+        self.controlMovement()
         
         return super().update()
 
--- game_name/extra.py
+++ game_name/extra.py
@@ -1,6 +1,11 @@
 from math import acos,degrees
 import pygame as pg
 
+def inInterval(interval: tuple, numb: float) -> bool:
+    '''Return true if the numb paramater is inside the interval parameter, like this: [a,b).'''
+    if numb >= interval[0] and numb < interval[1]: return True
+    return False
+
 def angle(vec1: pg.math.Vector2, vec2: pg.math.Vector2, smaller: bool=True) -> float:
     '''Returns the smallest angle between two vectors if smaller flag = True.\n'''
     vec1_norm = vec1.length()
